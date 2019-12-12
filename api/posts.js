729aee9dbd1c6bb12ab65182aeda8f890f057639
Kris Alcordo <kris.alcordo@majoritystrategies.com>
2019-12-11 23:37:47 -0500

    initial commit

---
+++ api/posts.js
@@ -0,0 +1,8 @@
+import GhostContentAPI from "@tryghost/content-api";
+
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+  url: 'https://api.krisalcordo.com',
+  key: '34e11206d02ba0d1399c792de3',
+  version: "v3"
+});

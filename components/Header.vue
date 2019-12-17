70d60e018c2cfcc9ccdac0a4d077e48354a17195
Kris Alcordo <kris.alcordo@majoritystrategies.com>
2019-12-16 23:22:50 -0500

    Added nav hamburger menu

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+      <div class="nav-container">
+          <div class="nav" v-bind:class="{active: isActive}" v-on:click="activeNav">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+      </div>
+      <h1>Kris Alcordo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            isActive: false
+        }
+    },
+    methods: {
+        activeNav: function(){
+            this.isActive = !this.isActive;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

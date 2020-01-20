f21d551b1be40eb5691ae5512b627bc94b099f80
Kris Alcordo <kris.alcordo@majoritystrategies.com>
2020-01-20 11:09:38 -0500

    Created breadcrumb not disabling use for now

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="crumb">
+      {{links}}
+        <ul>
+            <li v-for="(link, index) in links" v-bind:key="index">
+               <router-link to="">{{link}}</router-link>
+            </li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            links: Array
+        }
+    },
+    methods :{
+        convertString(link) {
+            let linkArray = link.split('/')
+            let returnArray = []
+            linkArray.forEach(element => {
+                if(element !== "") {
+                    returnArray.push(element)
+                }
+            });
+            // console.log(linkArray)
+            
+            return returnArray
+        }
+    },
+    mounted() {
+        this.links = this.convertString(window.location.pathname);
+        console.log(this.links)
+    }
+}
+</script>
+
+<style>
+    
+</style>

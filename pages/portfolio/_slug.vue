676ef1bad38b23e2c78d67aff5b49b608b46413c
Kris Alcordo <kjaonline@gmail.com>
2020-01-19 20:54:34 -0500

    Started work on portfolio items

---
+++ pages/portfolio/_slug.vue
@@ -0,0 +1,22 @@
+// query MyQuery {
+//   portfolio(where: {title: "Salvation Army Southern California"}) {
+//     edges {
+//       node {
+//         portfolio {
+//           tags {
+//             ... on Tag {
+//               id
+//               slug
+//             }
+//           }
+//           projectImages {
+//             sourceUrl
+//             caption(format: RENDERED)
+//           }
+//         }
+//         content(format: RENDERED)
+//         id
+//       }
+//     }
+//   }
+// }

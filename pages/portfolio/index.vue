d0e0bdbc2ae04b045f125eceab1f64be44543747
Kris Alcordo <kris.alcordo@majoritystrategies.com>
2020-05-24 22:15:10 -0400

    Created array of tags to query

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,13 +1,53 @@
 <template>
-  <div class="home-hero">
-	  <h1>Porfolio</h1>
+  <div class="portfolio">
+      <div class="header">
+        <h1>Portfolio</h1>
+      </div>
+      <div class="content">
+        <div class="tags">
+          <ul>
+            <li v-for="tags in portfoliotags" v-bind:key="tags.id" v-on:click="modifyTagsArray($event, tags.slug);">
+              {{ tags.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="posts">
+
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
+export default {
+  data: function() {
+    return{
+      activeTags: []
+    }
+  },
+  computed: {
+    ...mapState([
+      'portfoliotags'
+    ])
+  },
+  methods: {
+    modifyTagsArray: function(event, tag){
+      if(!this.activeTags.includes(tag)) {
+        this.activeTags.push(tag)
+      } else {
+        this.activeTags.splice(this.activeTags.indexOf(tag), 1)
+      }
+      console.log(this.activeTags)
+    }
+  }
+}
 
 </script>
 
 <style>
-
+  .tags ul li {
+    cursor: pointer;
+  }
 </style>
